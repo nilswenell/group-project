e3fe4a442879366a7e01a71601aadc7988f3bba0
nilswenell <nils.wenell@gmail.com>
2025-11-06 09:03:59 -0600

    more testing i guess

--- card_games/blackjack.py
+++ card_games/blackjack.py
@@ -1,3 +1,6 @@
 class BlackJackGame:
     def __init__(self):
         print("Test Blackjack")
+
+    def printsomething(something):
+        print(something + ' testing')
--- card_games/main.py
+++ card_games/main.py
@@ -1,3 +1,4 @@
 from blackjack import BlackJackGame
-if __name__=='__main__':
-    game1 = BlackJackGame()
+#if __name__=='__main__':
+game1 = BlackJackGame()
+BlackJackGame.printsomething('something')
