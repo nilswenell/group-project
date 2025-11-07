e16b4e6904900423cc7fbd9b6d2d15d153d0c9cb
nilswenell <nils.wenell@gmail.com>
2025-11-07 10:38:27 -0600

    make deck and blackjack value func

--- card_games/blackjack.py
+++ card_games/blackjack.py
@@ -1,6 +1,14 @@
+
 class BlackJackGame:
     def __init__(self):
         print("Test Blackjack")
 
     def printsomething(something):
         print(something + ' testing')
+
+import random
+
+suits = ['Hearts','Diamonds','Clubs','Spades']
+cards = ['Ace','2','3','4','5','6','7','8','9','10','Jack','Queen','King']
+deck = [(card,suit) for suit in suits for card in cards]
+print(deck)
