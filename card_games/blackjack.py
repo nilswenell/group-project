a6d62a3195bae2d23bb2711d6f32905ba4a36c3b
nilswenell <nils.wenell@gmail.com>
2025-11-07 10:44:10 -0600

    figuring out dealing

--- card_games/blackjack.py
+++ card_games/blackjack.py
@@ -8,7 +8,29 @@ class BlackJackGame:
 
 import random
 
+#making a deck
 suits = ['Hearts','Diamonds','Clubs','Spades']
 cards = ['Ace','2','3','4','5','6','7','8','9','10','Jack','Queen','King']
 deck = [(card,suit) for suit in suits for card in cards]
-print(deck)
+#print(deck)
+
+#return card value
+def get_card_value(card):
+    if card in ['Jack','Queen','King']:
+        return 10
+    elif card == 'Ace':
+        return 11
+    else:
+        return int(card)
+
+random.shuffle(deck)
+
+#deal cards
+
+hand_1 = []
+hand_2 = []
+
+def deal_card(hand):
+    random_card = random.choice(deck)
+    hand.append(random_card)
+    deck.remove(random_card)
