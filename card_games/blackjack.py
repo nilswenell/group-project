a719f7fe3993c6cc90e54d6a8f2b24324bb093b1
nilswenell <nils.wenell@gmail.com>
2025-11-18 11:54:23 -0600

    comments

--- card_games/blackjack.py
+++ card_games/blackjack.py
@@ -56,10 +56,14 @@ def player_turn(player):
 
 
 
-
+#initialize variables
 my_money = 1000
 endgame = False
+
+#start loop for whole game
 while my_money >0 and endgame ==False:
+    
+    #initialize more variables
     pot = 0
     bet = 0
     win = False
@@ -67,36 +71,44 @@ while my_money >0 and endgame ==False:
     my_hand = []
     dealer_hand = []
     
+    #ask for the first bet
     bet = input('bet:')
+    #always check if the player wants to quit
     if bet == 'quit':
         endgame = True
     else:
+        #adjust variables for start of turn
         bet = int(bet)
-        
-    
-
         my_money -= bet
         pot += bet
+        
+        #show player their money, the pot
         print(f'my money: {my_money}. pot: {pot}.')
+        
+        #deal player 2 cards, 1 to dealer
         deal_card(my_hand)
         deal_card(dealer_hand)
         deal_card(my_hand)
 
+        #show the hands and values
         print(f'your hand: {my_hand} value: {hand_value(my_hand)}')
         print(f'dealer hand: {dealer_hand}value: {hand_value(dealer_hand)}')
 
+
+        #check for automatic blackjack win
         if hand_value(my_hand) == 21:
             my_money += (pot*2)
             pot = 0
             bet = 0
             print('blackjack!')
             print(f'my money: {my_money}.')
+        
         else:
-
+            #begin loop of an individual turn
             endloop = False
             while endloop == False:
         
-
+                #check for blackjack every time
                 if hand_value(my_hand) == 21:
                     my_money += (pot*2)
                     pot = 0
@@ -104,7 +116,11 @@ while my_money >0 and endgame ==False:
                     print('blackjack!')
                     print(f'my money: {my_money}.')
                     endloop=True
+                
+                #ask hit or stand
                 player_turn(my_hand)
+                
+                #check for bust
                 if hand_value(my_hand)>21:
         
                     pot = 0
@@ -113,13 +129,15 @@ while my_money >0 and endgame ==False:
                     print('bust')
                     print(f'my money: {my_money}.')
                     endloop=True
+                
                 else:    
        
-
+                    #dealer hits on 17, unless player has more than 17
                     while hand_value(dealer_hand)<17 or hand_value(dealer_hand)<hand_value(my_hand):
                         deal_card(dealer_hand)
                         print(f'dealer hand: {dealer_hand}value: {hand_value(dealer_hand)}')
                 
+                    #if tied, push
                     if hand_value(dealer_hand)>16 and hand_value(dealer_hand) == hand_value(my_hand):
                         my_money += pot
                         pot = 0
@@ -127,7 +145,8 @@ while my_money >0 and endgame ==False:
                         print('push')
                         print(f'my money: {my_money}.')
                         endloop=True
-
+                    
+                    #dealer bust
                     elif hand_value(dealer_hand)>21:
                         my_money += (pot*2)
                         pot = 0
@@ -138,7 +157,9 @@ while my_money >0 and endgame ==False:
                     else:
                         print(f'your hand: {my_hand} value: {hand_value(my_hand)}')
                         print(f'dealer hand: {dealer_hand}value: {hand_value(dealer_hand)}')
+
+#result of game
 if my_money<1:
     print('you lost all your money!!!!')                    
 else:
-    print(f'you ended with {my_money} dollars.')
+    print(f'you ended with ${my_money}.')
